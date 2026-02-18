8c6607f98fc6761aa3c424ddd8519e8958f64c72
neerajkumarcoder <neerajkumarcbse@gmail.com>
2026-02-18 12:58:27 +0530

    Fixed CORS production URL

--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+   origin: [
+    "http://localhost:5173",
+    "https://job-portal-two-mu-86.vercel.app"
+  ],
   credentials: true,
 };
 app.use(cors(corsOptions));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 //api's
 app.use("/api/v1/user",userRoute);
